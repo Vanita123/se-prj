d62f3ce748c08a1065dc342363ff0bddcb19cea8
lghanta <lghanta@iu.edu>
2022-09-26 21:11:15 -0400

    Tejaswy: Initial login page, sign-in and google sign-in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import {Login} from './login';
+import {Signin} from './signin';
+import {Home} from './home';
+import {GoogleSignIn} from './gsignin';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/gsignin" element={<GoogleSignIn />} />
+      </Routes>
     </div>
+    
   );
 }
+
+
 
 export default App;
