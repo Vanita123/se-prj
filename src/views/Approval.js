c709a793999c9bf8ba556b41cc23e5556aa9ec7f
lghanta <lghanta@iu.edu>
2022-11-27 17:07:33 -0500

    Tables in required pages

--- src/views/Approval.js
+++ src/views/Approval.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 export default function Approval(){
-    const [approvals, setApprovals] = useState();
+    const [approvals, setApprovals] = useState([]);
 
     useEffect(()=>{
         axios.post("http://localhost:3000/approval", {
@@ -9,15 +9,45 @@ export default function Approval(){
                 }).then((response) => {
                  if(response.data){
                    console.log(response);
+                   setApprovals(response.data);
+                   console.log(approvals);
                  }
                 });
         
-        setApprovals();
-        console.log(approvals);
-    })
+       
+    },[]);
+
+    function RenderTable(){
+        const tbodyData = approvals;
+        const theadData = Object.keys(tbodyData[0]);
+
+                return (
+            <table>
+                <thead>
+                <tr>
+                {theadData.map(heading => {
+                return <th key={heading}>{heading}</th>
+                })}
+                </tr>
+                </thead>
+                <tbody>
+                {tbodyData.map((row, index) => {
+                return <tr key={index}>
+                {theadData.map((key, index) => {
+                return <td key={row[key]}>{row[key]}</td>
+                })}
+                </tr>;
+                })}
+              {/* Add approve button */}
+                </tbody>
+                </table>
+           )
+    }
+
     return (
         <div>
-            <h2>Approvals page - Admin view</h2>
+            <h2>Approve the pets</h2>
+            <RenderTable/>
         </div>
     )
 }
--- src/views/Refund.js
+++ src/views/Refund.js
@@ -2,25 +2,54 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Refund(){
-    const [refund, setRefund] = useState();
+    const [refund, setRefund] = useState([]);
 
     useEffect(()=>{
         //get refunds of the loggedin user
-        setRefund();
-        console.log(refund);
+       
 
         axios.post("http://localhost:3000/refund", {
             
         }).then((response) => {
          if(response.data){
-           console.log(response);
+            setRefund(response.data);
+            console.log(refund);
          }
         });
 
-    })
+    },[]);
+
+    function RenderTable(){
+        const tbodyData = refund;
+        const theadData = Object.keys(tbodyData[0]);
+
+                return (
+            <table>
+                <thead>
+                <tr>
+                {theadData.map(heading => {
+                return <th key={heading}>{heading}</th>
+                })}
+                </tr>
+                </thead>
+                <tbody>
+                {tbodyData.map((row, index) => {
+                return <tr key={index}>
+                {theadData.map((key, index) => {
+                return <td key={row[key]}>{row[key]}</td>
+                })}
+                </tr>;
+                })}
+              
+                </tbody>
+                </table>
+           )
+    }
+
     return (
         <div>
-            <h2>Refund page</h2>
+            <h3>Refunds page</h3>
+            <RenderTable/>
         </div>
     )
 }
